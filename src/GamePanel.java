f61b925320666937a50690252bd72718a907bc29
Midgar77 <ajg1179@gmail.com>
2014-11-26 12:58:21 -0500

    Updated GamePanel.java for Score & Directions
    Changed the paintComponent method so that a score is constantly shown in the game in the top left hand corner of the screen.
    This score is incremented in the checkForCollisions method in Game.java.
    
    Additionally, I added directions to the player at the beginning of the game before the player starts (tells the player what buttons can be pressed) and when the game ends (tells the player how to restart the game).
    
    Before this commit, I had no idea (without looking in the code or pressing random keys) how to start the game. So, I figured these directions would be useful to the player.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -28,6 +28,28 @@ public class GamePanel extends JPanel implements Runnable {
                 g2D.drawImage(r.image, r.transform, null);
             else
                 g.drawImage(r.image, r.x, r.y, null);
+                
+                
+        g2D.setColor(Color.ORANGE);
+        g2D.setFont(new Font("TimesRoman", Font.PLAIN, 50));
+        g2D.drawString(Integer.toString(game.getScore()), 10, 40);
+        
+        if(!game.gameStarted()){
+        	g2D.setColor(Color.BLACK);
+            g2D.setFont(new Font("TimesRoman", Font.PLAIN, 40));
+        	g2D.drawString("Press Space to start", 100, 250);
+        	
+        	g2D.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        	g2D.drawString("Press P to pause game", 150, 300);
+        	g2D.drawString("Press R to reset game", 150, 320);
+        }
+        
+        if(game.gameOver()){
+        	g2D.setColor(Color.RED);
+            g2D.setFont(new Font("TimesRoman", Font.PLAIN, 40));
+            g2D.drawString("Game Over!", 150, 200);
+            g2D.drawString("Press R to restart game", 80, 250);
+        }
     }
     
     public void run() {
